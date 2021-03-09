a991e970e23d35b8004488a88a60a5928deb1604
alexmorenograu <61759297+alexmorenograu@users.noreply.github.com>
2021-03-09 08:40:17 +0100

    encrypted

--- encrypted.js
+++ encrypted.js
@@ -5,13 +5,9 @@ const config = require('./config');
 const encryptPassword = async(password) => {
         const { body } = await got.get('http://app.etiquetaselectronicas.com:9999/user/getErpPublicKey', {
         });
-        const publicKey=`-----BEGIN PUBLIC KEY-----\n${JSON.parse(body).data}\n-----END PUBLIC KEY-----`;
-        const externKey = { 
-            key: publicKey,
-            padding: crypto.constants.RSA_PKCS1_PADDING 
-        };
+        let publicKey=`-----BEGIN PUBLIC KEY-----\n${JSON.parse(body).data}\n-----END PUBLIC KEY-----`;
         let buffer = Buffer.from(password);
-        return crypto.publicEncrypt(externKey, buffer).toString("base64");
-};
+        return crypto.publicEncrypt({ key: publicKey, padding: crypto.constants.RSA_PKCS1_PADDING }, buffer).toString("base64");
+    };
 
 exports.encryptPassword = encryptPassword;
