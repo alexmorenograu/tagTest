2e8648d27ed1cd900e96013311b0014751ef6b41
alexmorenograu <61759297+alexmorenograu@users.noreply.github.com>
2021-03-09 08:04:19 +0100

    ecrypted

--- encrypted.js
+++ encrypted.js
@@ -3,6 +3,7 @@ const got = require('got');
 const config = require('./config');
 
 const encryptPassword = async(password) => {
+        const got = require('got');
         const { body } = await got.get('http://app.etiquetaselectronicas.com:9999/user/getErpPublicKey', {
         });
         let publicKey=`-----BEGIN PUBLIC KEY-----\n${JSON.parse(body).data}\n-----END PUBLIC KEY-----`;
